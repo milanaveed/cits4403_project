698993eb5e78e5581e9696bbfd2e0255c7e565ad
oddnaveed <84265464+oddnaveed@users.noreply.github.com>
2023-09-24 13:51:40 +0800

    Add comments for figure_1.py

--- ants-code/figure_1.py
+++ ants-code/figure_1.py
@@ -4,22 +4,23 @@ from pathlib import Path
 import config as cf
 from ants import main, plot_path
 
-NAME = "main"
-NUM_STEPS = 2000
-INIT_ANTS = 70
-MAX_ANTS = 70
+NAME = "main"  # Name for this simulation
+NUM_STEPS = 2000  # Number of simulation steps
+INIT_ANTS = 70  # Initial number of ants
+MAX_ANTS = 70  # Maximum number of ants in the simulation
 
-Path("imgs").mkdir(parents=True, exist_ok=True)
+Path("imgs").mkdir(parents=True, exist_ok=True)  # Create a directory for saving images if doesn't exist
 
-# standard prior
+# Define a standard prior for observations
 PRIOR_TICK = 1
 C = np.zeros((cf.NUM_OBSERVATIONS, 1))
 prior = 0
-for o in range(cf.NUM_OBSERVATIONS):
+for o in range(cf.NUM_OBSERVATIONS):  # Assign values to the observation prior
     C[o] = prior
     prior += PRIOR_TICK
 
 if __name__ == "__main__":
+    # Run the main simulation
     num_round_trips, paths, coeff = main(
         num_steps=NUM_STEPS,
         init_ants=INIT_ANTS,
@@ -30,11 +31,15 @@ if __name__ == "__main__":
         name=NAME,
         ant_only_gif=False,
     )
+
+    # Print simulation results
     print(f"num_round_trips {num_round_trips} / coeff {coeff / MAX_ANTS}")
+
+    # Save simulation results to a text file
     f = open(f"imgs/{NAME}.txt", "w")
     f.write(f"num_round_trips {num_round_trips} / coeff {coeff / MAX_ANTS}")
     f.close()
 
-    
+    # Generate and save path images for visualization
     for i in range(len(paths)):
         plot_path(np.random.choice(paths), f"imgs/path_{i}.png")
